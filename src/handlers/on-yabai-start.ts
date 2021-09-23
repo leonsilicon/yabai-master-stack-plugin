8c55b0f1086a26d71996cd2d7c94491d86cfcfbe
Leon Si <leonzalion@gmail.com>
2021-09-22 22:24:31 -0400

    reset state

--- src/handlers/on-yabai-start.ts
+++ src/handlers/on-yabai-start.ts
@@ -1,9 +1,13 @@
+import { readState, writeState } from '../state';
 import { createWindowsManager } from '../utils';
 import { getFocusedDisplay } from '../utils/display';
 import { releaseLock } from '../utils/lock';
 
 try {
 	releaseLock();
+	const state = readState();
+	state.numMainWindows = 1;
+	writeState(state);
 	const wm = createWindowsManager({ display: getFocusedDisplay() });
 	wm.updateWindows();
 } catch {}
