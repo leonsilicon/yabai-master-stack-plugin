cc099ad49ad3861de61dbddd960cdaa10fbe7f0c
Leon Si <leonzalion@gmail.com>
2021-10-13 13:18:15 -0400

    stricter limits on the number of master windosw

--- src/fns/decrease-master-window-count.ts
+++ src/fns/decrease-master-window-count.ts
@@ -4,11 +4,14 @@ import { main } from '../utils/main';
 
 main(async () => {
 	const { wm, state, display } = await createInitializedWindowsManager();
-	// Update the state
-	state[display.id].numMasterWindows -= 1;
-	writeState(state);
-	console.log('Decreasing master window count.');
-	await wm.updateWindows({
-		targetNumMasterWindows: state[display.id].numMasterWindows,
-	});
+
+	if (state[display.id].numMasterWindows > 1) {
+		// Update the state
+		state[display.id].numMasterWindows -= 1;
+		writeState(state);
+		console.log('Decreasing master window count.');
+		await wm.updateWindows({
+			targetNumMasterWindows: state[display.id].numMasterWindows,
+		});
+	}
 });
--- src/fns/increase-master-window-count.ts
+++ src/fns/increase-master-window-count.ts
@@ -4,7 +4,7 @@ import { main } from '../utils/main';
 
 main(async () => {
 	const { wm, display, state } = await createInitializedWindowsManager();
-	if (state[display.id].numMasterWindows < wm.windowsData.length) {
+	if (state[display.id].numMasterWindows < wm.windowsData.length - 1) {
 		state[display.id].numMasterWindows += 1;
 		writeState(state);
 		console.log('Increasing master window count.');
