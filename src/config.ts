0da184fc0cf4aeb4de69dec4e3e23359a04bec4f
Leon Si <leonzalion@gmail.com>
2021-10-15 19:56:07 -0400

    added replace file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,3 @@
 import 'dotenv/config';
 
-if (process.env.YABAI_PATH === undefined) {
-	throw new Error('YABAI_PATH not specified in enviroment/.env file!');
-}
-
-export const yabaiPath = process.env.YABAI_PATH;
+export const yabaiPath = process.env.YABAI_PATH!;
