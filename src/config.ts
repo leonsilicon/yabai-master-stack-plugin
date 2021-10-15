2238422cd29b6adf8e09661c7dc142dac2bcf881
Leon Si <leonzalion@gmail.com>
2021-10-15 19:14:55 -0400

    move yabai path to env variable

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,7 @@
-export const yabaiPath = '/usr/local/bin/yabai';
+import 'dotenv/config';
+
+if (process.env.YABAI_PATH === undefined) {
+	throw new Error('YABAI_PATH not specified in enviroment/.env file!');
+}
+
+export const yabaiPath = process.env.YABAI_PATH;
