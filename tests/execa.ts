97a38a9c5bd99f153a333f95da61b2179d6f978e
Leon Si <contact@leonzalion.com>
2021-11-24 19:48:47 -0500

    better perf

--- tests/execa.ts
+++ tests/execa.ts
@@ -20,30 +20,33 @@ async function execute(command: string) {
 async function main() {
 	const suite = new Benchmark.Suite();
 
-	const command = '-m query --windows --window';
+	const command = ' -m query --windows --window';
+	const parsedCommand = parse(command) as string[];
 
 	suite
 		.add(
 			'execa + getYabaiOutput',
 			p(async () => {
-				const yabaiProcess = execa(yabaiPath, parse(command) as string[]);
+				const yabaiProcess = execa(yabaiPath, parsedCommand);
 				const _output = await getYabaiOutput(yabaiProcess);
 			})
 		)
 		.add('execa + commandSync', () => {
-			const _output = execa.commandSync(`${yabaiPath} ${command}`).stdout;
+			// eslint-disable-next-line prefer-template
+			const _output = execa.commandSync(yabaiPath + command).stdout;
 		})
 		.add(
 			'execa + command',
 			p(async () => {
-				const execaResult = await execa.command(`${yabaiPath} ${command}`);
+			// eslint-disable-next-line prefer-template
+				const execaResult = await execa.command(yabaiPath + command);
 				const _output = execaResult.stdout;
 			})
 		)
 		.add(
 			'child_process.exec',
 			p(async () => {
-				const _output = await execute(`${yabaiPath} ${command}`);
+				const _output = await execute(yabaiPath + command);
 			})
 		)
 		.on('cycle', (event: any) => {
