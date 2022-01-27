70b1024852f0a6b72736ae8ded904bde93a52f89
Leon Si <contact@leonzalion.com>
2022-01-26 19:38:26 -0500

    fix

--- test/perf.ts
+++ test/perf.ts
@@ -1,26 +1,20 @@
-import 'dotenv/config';
-
 import Benchmark from 'benchmark';
 
-import { setDebug } from '../src/config';
-import { createInitializedWindowsManager } from '../src/utils/window';
-import { p } from './utils';
+import { p } from './utils.js';
+import { createInitializedWindowsManager } from '~/utils/index.js';
 
-setDebug(false);
+const suite = new Benchmark.Suite();
 
-async function main() {
-	const suite = new Benchmark.Suite();
+const { wm, state } = await createInitializedWindowsManager();
+const window = wm.getFocusedWindow()!;
+const stackWindows = wm.getStackWindows();
 
-	const { wm, state } = await createInitializedWindowsManager();
-	const window = wm.getFocusedWindow()!;
-	const stackWindows = wm.getStackWindows();
-
-	// prettier-ignore
-	suite
+// prettier-ignore
+suite
 		.add('columnizeStackWindows', p(() => wm.columnizeStackWindows))
-		.add('createStack', p(() => wm.createStack()))
+		.add('createStack', p(async () => wm.createStack()))
 		.add('doesStackExist', p(() => wm.doesStackExist()))
-		.add('executeYabaiCommand', p(() => wm.executeYabaiCommand('-m query --window')))
+		.add('executeYabaiCommand', p(async () => wm.executeYabaiCommand('-m query --window')))
 		.add('getBottomMasterWindow', p(() => wm.getBottomMasterWindow()))
 		.add('getBottomStackWindow', p(() => wm.getBottomStackWindow()))
 		.add('getBottomWindow', p(() => wm.getBottomWindow(stackWindows)))
@@ -38,23 +32,20 @@ async function main() {
 		.add('getWidestMasterWindow', p(() => wm.getWidestMasterWindow()))
 		.add('getWidestStackWindow', p(() => wm.getWidestStackWindow()))
 		.add('getWindowData', p(() => wm.getWindowData({ windowId: window.id.toString() })))
-		.add('initialize', p(() => wm.initialize()))
+		.add('initialize', p(async () => wm.initialize()))
 		.add('isBottomWindow', p(() => wm.isBottomWindow(stackWindows, window)))
 		.add('isMasterWindow', p(() => wm.isMasterWindow(window)))
 		.add('isMiddleWindow', p(() => wm.isMiddleWindow(window)))
 		.add('isStackWindow', p(() => wm.isStackWindow(window)))
 		.add('isTopWindow', p(() => wm.isTopWindow(stackWindows, window)))
-		.add('isValidLayout', p(() => wm.isValidLayout()))
+		.add('isValidLayout', p(async () => wm.isValidLayout()))
 		.add('isWindowTouchingLeftEdge', p(() => wm.isWindowTouchingLeftEdge(window)))
-		.add('moveWindowToMaster', p(() => wm.moveWindowToMaster(window)))
-		.add('moveWindowToStack', p(() => wm.moveWindowToStack(window)))
-		.add('refreshWindowsData', p(() => wm.refreshWindowsData()))
-		.add('updateWindows', p(() => wm.updateWindows({ targetNumMasterWindows: 1 })))
-		.add('validateState', p(() => wm.validateState(state)))
+		.add('moveWindowToMaster', p(async () => wm.moveWindowToMaster(window)))
+		.add('moveWindowToStack', p(async () => wm.moveWindowToStack(window)))
+		.add('refreshWindowsData', p(async () => wm.refreshWindowsData()))
+		.add('updateWindows', p(async () => wm.updateWindows({ targetNumMasterWindows: 1 })))
+		.add('validateState', p(() => { wm.validateState(state); }))
 		.on('cycle', (event: any) => {
 			console.log(String(event.target));
 		})
 		.run({ async: true });
-}
-
-void main();
