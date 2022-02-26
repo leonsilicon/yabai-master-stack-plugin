6a5ae94f77fb37755c640937fcf08807913b1b44
Leon Si <contact@leonzalion.com>
2022-02-26 01:28:59 -0500

    chore: updating deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 const path = require('path');
-const createAliases = require('@leonzalion/configs/eslint/alias');
+const createAliases = require('@leonzalion/configs/eslint/alias.cjs');
 
 module.exports = {
-	extends: [require.resolve('@leonzalion/configs/eslint')],
+	extends: [require.resolve('@leonzalion/configs/eslint.cjs')],
 	parserOptions: {
 		project: [path.resolve(__dirname, './tsconfig.eslint.json')],
 	},
--- scripts/build.ts
+++ scripts/build.ts
@@ -2,7 +2,6 @@ import { execaCommandSync as exec } from 'execa';
 import { rmDist, chProjectDir, copyPackageFiles } from 'lion-system';
 
 chProjectDir(import.meta.url);
-
 rmDist();
 exec('tsc');
 exec('tsc-alias');
